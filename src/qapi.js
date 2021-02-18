f7bd0527eac49a24a5e275645d03b9bd22529c83
saksham02112000 <55004186+saksham02112000@users.noreply.github.com>
2021-02-18 13:48:50 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"602e1b7b8db767001e87931d",
+				"602e1b7b8db767001e87931f"
+			]
+		},
+		"602e1b7b8db767001e87931d": {
+			"id": "602e1b7b8db767001e87931d",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"602e1b7b8db767001e87931f": {
+			"id": "602e1b7b8db767001e87931f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
